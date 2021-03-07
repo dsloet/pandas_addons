42c74fb3bb384443ef23397a61a97d8615968549
Diederik Sloet <diederiksloet@Diederiks-MacBook-Pro.local>
2021-03-07 10:12:56 +0100

    indents in doc string

--- addons.py
+++ addons.py
@@ -5,16 +5,16 @@ def select(df, column_name, value):
     Intuitive way of applying filters to a dataframe
 
     Parameters
-        ----------
-        column_name : name of the columns
-            column name for the function to be applied on.
-        value : value to filter on
-            value(s) to select within the column
-        
-        Returns
-        -------
-        dataframe with only the rows where the filter
-        found a match
+    ----------
+    column_name : name of the columns
+        column name for the function to be applied on.
+    value : value to filter on
+        value(s) to select within the column
+    
+    Returns
+    -------
+    dataframe with only the rows where the filter
+    found a match
 
 
     """
