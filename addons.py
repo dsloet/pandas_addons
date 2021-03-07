f6a58ae3f0635179a3072fd2b71058157cabf223
Diederik Sloet <diederiksloet@Diederiks-MacBook-Pro.local>
2021-03-07 11:46:45 +0100

    adding median_abs_dev

--- addons.py
+++ addons.py
@@ -1,4 +1,6 @@
 from pandas.core.base import PandasObject
+import numpy as np
+
 
 def select(df, column_name, value):
     """
@@ -10,14 +12,51 @@ def select(df, column_name, value):
         column name for the function to be applied on.
     value : value to filter on
         value(s) to select within the column
-    
+
     Returns
     -------
     dataframe with only the rows where the filter
     found a match
 
-
     """
     return df[df[column_name] == value]
 
+
+def median_abs_dev(df, axis=None, skipna=None):
+    """
+    Calculate the median absolute deviation of the variable X.
+    Robust way of MAD when there are outliers in the data. Can be used in 
+    combination with calcuting the robust z-score.
+
+    Calculation of median absolute deviation:
+
+    median_abs_dev = median{|xi - median(X)|}
+
+    Parameters
+    ----------
+    axis : {axis_descr}
+        Axis for the function to be applied on.
+    skipna : bool, default None
+        Exclude NA/null values when computing the result.
+
+    Returns
+    -------
+    median absolute deviation of the Variable X
+
+    """
+    if skipna is None:
+        skipna = True
+    if axis is None:
+        axis = df._stat_axis_number
+
+    data = df._get_numeric_data()
+    # print("median of the sample = ", data.median())
+    if axis == 0:
+        demedianed = data - data.median(axis=0)
+    else:
+        demedianed = data.sub(data.median(axis=1), axis=0)
+    return np.abs(demedianed).median(axis=axis, skipna=skipna)
+
+
 PandasObject.select = select
+PandasObject.median_abs_dev = median_abs_dev
--- test.py
+++ test.py
@@ -2,10 +2,18 @@ import pandas as pd
 from addons import *
 
 data = {
-    "column1":["foo", "bar", "bar"],
-    "column2":[1, 2, 3]
+    "column1": ["foo", "bar", "bar", "foobar"],
+    "column2": [1, 2, 3, 4],
+    "column3": [1, np.nan, 500, 150000]
 }
 
 df = pd.DataFrame(data)
 
-print(df.select("column1", "bar"))
+
+# print(df.select("column1", "bar"))
+
+print(df["column2"].mad())
+print(df["column2"].median_abs_dev())
+print(df["column3"].mad())
+print(df["column3"].median_abs_dev())
+print(df["column3"].median_abs_dev(skipna=False))
